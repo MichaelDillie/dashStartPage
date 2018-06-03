63087f3eb1c1b56bd6e67a36518b641187759a52
Michael Dillie <michaeldillie@gmail.com>
2018-06-03 00:37:37 -0500

    Date and Time on DOM

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+    "November", "December"
+  ];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  
+  var currentdate = new Date();
+  var currentMont = months[currentdate.getMonth()];
+  var currentDay = currentdate.getDate();
+  var currentWeekDay = days[currentdate.getDay()];
+  var date = currentWeekDay + ", " + currentMont + " " + currentDay;
+  console.log(date);
 
+  var currentHour = currentdate.getHours();
+  var currentMin = currentdate.getMinutes();
+  var time = currentHour + ":" + currentMin;
+  console.log(time);
+
+  var dateDiv = $("#date");
+  var timeDiv = $("#time");
+
+  function test() {
+    dateDiv.text(date);
+    timeDiv.text(time);
+  }
+  test();
 });
